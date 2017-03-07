19d14c358a7320637345dac6e7439ca2605b9c05
GiannaP <pauling@student.ethz.ch>
2017-03-07 17:20:49 +0100

    add login for a single admin user

--- main.py
+++ main.py
@@ -1,36 +1,76 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from apps.models import Participants, TimeSlots
 import requests
 import datetime
+from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
+# create and config app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:toor@localhost/Speeddating'
+app.config.from_pyfile("./apps/config.py")
+app.secret_key = app.config['APP_SECRET']
+app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI']
 db = SQLAlchemy(app)
 
 # Creates tables only when they don't already exist so we can just leave this here
 db.create_all()
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+    if username not in app.config['USERS'].keys():
+        return
+
+    user = User()
+    user.id = username
+    return user
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        result = request.form
-
-        # Send POST request with parameters 'username' and 'password'. Verification has to be off since
-        # SSL certs are self-signed.
-        ack = requests.post("https://amiv-apidev.vsos.ethz.ch/sessions", data={"username" : str(result['nethz']), "password" : str(result['password'])}, verify=False)
-
-        if ack.status_code == 201:
-            return render_template("admin.html")
+    if request.method == 'GET':
         return render_template('login.html')
-    return render_template('login.html')
+    form_username = request.form['username']
+    if form_username in app.config['USERS'].keys():
+        if app.config['USERS'][form_username] == request.form['password']:
+            user = User()
+            user.id = form_username
+            login_user(user)
+            return redirect(url_for('admin'))
+    return 'Bad login'
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+
+@app.route('/admin', methods=["GET", "POST"])
+@login_required
+def admin():
+    #return 'Logged in as: ' + current_user.id
+    return render_template('admin.html')
+
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
@@ -59,7 +99,6 @@ def signup():
         return render_template('success.html')
 
     return render_template('signup.html')
-
 
 
 if __name__ == '__main__':
