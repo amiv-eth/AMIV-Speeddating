1bf5c35f24ded37d02bb6c073da58f6cd0061330
GiannaP <pauling@student.ethz.ch>
2017-03-28 16:40:51 +0200

    add only one event active, only active event can be open functionality

--- app/functions.py
+++ app/functions.py
@@ -3,7 +3,15 @@ from app.models import Participants, TimeSlots, Events
 def event_change_signup_status(session, event_id, open):
     try:
         event = session.query(Events).filter_by(ID = event_id).first()
-        event.SignupOpen= int(open)
+        
+        if event.Active == 1:
+            if open == 1:
+                event.SignupOpen= int(open)
+            else:
+                event.SignupOpen= int(open)
+        else:
+            if open == 0:
+                event.SignupOpen= int(open)
         session.commit()
         return True
     except Exception as e:
@@ -12,10 +20,26 @@ def event_change_signup_status(session, event_id, open):
 
 def event_change_active_status(session, event_id, active):
     try:
-        event = session.query(Events).filter_by(ID = event_id).first()
-        event.Active= int(active)
+        nr_active_events = session.query(Events).filter(Events.Active == active).count()
+
+        if active == 1:
+            if nr_active_events > 0:
+                events = session.query(Events).all()
+                for e in events:
+                    event_change_signup_status(session, e.ID, 0)
+                    e.Active = 0
+                    
+            event = session.query(Events).filter(Events.ID == event_id).first()
+            event.Active = int(active)
+
+        if active == 0:
+            event_change_signup_status(session, event_id, 0)
+            event = session.query(Events).filter(Events.ID == event_id).first()
+            event.Active = int(active)
+
         session.commit()
         return True
+
     except Exception as e:
         print(e)
         return False
