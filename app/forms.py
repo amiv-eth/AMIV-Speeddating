51776c1d0a309f327e0b63a4adce87773ec40637
GiannaP <pauling@student.ethz.ch>
2017-03-28 16:44:04 +0200

    add requirement sign (*) for signup form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from wtforms import Form, TextField, DateField, RadioField, StringField, IntegerField, PasswordField, SelectField, validators
 from wtforms_components import TimeField
+from app.models import Participants, TimeSlots, Events
 
 class LoginForm(Form):
     username = StringField('Username', [validators.DataRequired()])
@@ -20,17 +21,17 @@ class CreateTimeSlotForm(Form):
     agerange = RadioField('Age-Range:', [validators.DataRequired()], choices=[('0','<22'),('1','22 - 25'),('2','> 25')])
 
 
-class SignupForm(Form):     
-    name = StringField('Nachname', [validators.DataRequired()])
-    prename = StringField('Vorname', [validators.DataRequired()])
-    email = TextField('E-Mail Adresse', validators=[validators.DataRequired(), validators.Email()])
-    mobilenr = StringField( 'Handynummer', [validators.DataRequired()])
-    address = TextField('Adresse', [validators.DataRequired()])
-    birthday = DateField('Geburtstag',[validators.DataRequired()], format='%Y-%m-%d')
-    gender = RadioField('Geschlecht:', [validators.DataRequired()], choices=[('1','w'),('0','m')])
-    studycourse = StringField('Studiengang', [validators.DataRequired()])
-    studysemester = StringField('Semester', [validators.DataRequired()])
+class SignupForm(Form):
+    name = StringField('Nachname (*)', [validators.DataRequired()])
+    prename = StringField('Vorname (*)', [validators.DataRequired()])
+    email = TextField('E-Mail Adresse (*)', validators=[validators.DataRequired(), validators.Email()])
+    mobilenr = StringField( 'Handynummer (*)', [validators.DataRequired()])
+    address = TextField('Adresse (*)', [validators.DataRequired()])
+    birthday = DateField('Geburtstag (*)',[validators.DataRequired()], format='%Y-%m-%d')
+    gender = RadioField('Geschlecht (*)', [validators.DataRequired()], choices=[('1','w'),('0','m')])
+    studycourse = StringField('Studiengang (*)', [validators.DataRequired()])
+    studysemester = StringField('Semester (*)', [validators.DataRequired()])
     perfectdate = StringField('Das perfekte Date?', [validators.Optional()])
     fruit = StringField('Lieblingsfrucht?', [validators.Optional()])
 
-    availableslots = RadioField('Verfügbare Daten (Bitte achte auf die Altersgruppe)', validators = [validators.Optional()], coerce=int)
+    availableslots = RadioField('Verfügbare Daten (*) (Bitte achte auf die Altersgruppe)', validators = [validators.DataRequired()], coerce=int)
