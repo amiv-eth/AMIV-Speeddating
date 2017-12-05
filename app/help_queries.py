2dfa27b2c301068ad5c0107bc6b0e8052bb1e13a
Mathis Dedial <mathis@dedial.net>
2017-12-05 01:37:57 +0100

    Extend 'participants_in_slot' to add the possibility to filter by confirmation status

--- app/help_queries.py
+++ app/help_queries.py
@@ -4,18 +4,21 @@ Collection of frequently used queries
 from app.models import Participants
 
 
-def participants_in_slot(slot, gender=None):
+def participants_in_slot(slot, gender=None, confirmed=None):
     """ Returns a list of two lists, one containing the participants who made the slot,
     one containing those on the waiting list """
-    # Only select Participants who have confirmed their participation
+    # Select participants signed up for slot in ascending order by signup timestamp
     participants = Participants.query.filter(
-        Participants.available_slot == slot.id,
-        Participants.confirmed
+        Participants.available_slot == slot.id
     ).order_by(Participants.creation_timestamp)
 
     # Optional gender filtering
     if gender is not None:
         participants = participants.filter(Participants.gender == gender)
+
+    # Optional filtering by confirmation status
+    if confirmed is not None:
+        participants = participants.filter(Participants.confirmed == confirmed)
 
     participants_list = participants.all()
     nr_couples = slot.nr_couples
